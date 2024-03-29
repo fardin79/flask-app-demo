9d3602e696e0d917f49a9c53bc061d8d904e6fb9
Fardin <fardeenrangrezz4@gmail.com>
2024-03-29 17:14:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 from flask import Flask
+from gevent.pywsgi import WSGIServer
  
 # Flask constructor takes the name of 
 # current module (__name__) as argument.

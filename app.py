6589eca9d79c19fe9822e6b758b2d22526512e9c
Fardin <fardeenrangrezz4@gmail.com>
2024-03-29 17:12:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,5 @@ if __name__ == '__main__':
  
     # run() method of Flask class runs the application 
     # on the local development server.
-    app.run()
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)

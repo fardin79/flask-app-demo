5d8b7ca0c92572980c4c8151ccce738fd986cff4
Fardin <fardeenrangrezz4@gmail.com>
2024-03-29 17:40:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
-
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
 from flask import Flask
-from gevent.pywsgi import WSGIServer
- 
-# Flask constructor takes the name of 
-# current module (__name__) as argument.
+from waitress import serve
+
 app = Flask(__name__)
- 
-# The route() function of the Flask class is a decorator, 
-# which tells the application which URL should call 
-# the associated function.
-@app.route('/')
-# ‘/’ URL is bound with hello_world() function.
-def hello_world():
-    return 'Hello World'
- 
-# main driver function
+
+@app.route('/hi')
+def index():
+    return 'Hello, World with waitress!!!'
+
 if __name__ == '__main__':
- 
-    # run() method of Flask class runs the application 
-    # on the local development server.
-    http_server = WSGIServer(('', 5000), app)
-    http_server.serve_forever()
+    serve(app, host='0.0.0.0', port=50100, threads=1, url_prefix="/my-app")

3ea24a7fb966e59e28b6e8689d42f8386570e2e3
Fardin <fardeenrangrezz4@gmail.com>
2024-03-29 17:46:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from waitress import serve
 
 app = Flask(__name__)
 
-@app.route('/hi')
+@app.route('/')
 def index():
     return 'Hello, World with waitress!!!'
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=50100, threads=1, url_prefix="/my-app")
+    serve(app, host='0.0.0.0', port=50100, threads=1)
